8600841b95ac33b796d3f4f93f9e7a0698012cf3
Kleiton Andrade <Kleiton1002andrade@gmail.com>
2024-02-07 23:54:30 -0100

    UI v0.1

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import flet_contrib.color_picker as ft_color_picker
 def main(page: ft.Page):
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.window_max_width = 500
+    page.theme_mode = ft.ThemeMode.DARK
 
     global width 
     global height 
@@ -16,21 +17,32 @@ def main(page: ft.Page):
     global color_units
 
     first_row = ft.Container(
+        margin=ft.margin.only(top=5, left=15,right=15,bottom=15),
         content=ft.Row(
             alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
             controls=[
-                ft.Row(
-                    alignment=ft.MainAxisAlignment.SPACE_EVENLY,
-                    controls=[   
-                        ft.Icon(name=ft.icons.CIRCLE, size=10, color=ft.colors.GREEN),
-                        ft.TextButton(text="Device ID"),
-                    ]
-                    
+                ft.TextButton(
+                    content=ft.Container(
+                        content=ft.Row(
+                            [
+                                ft.Icon(name="circle",size=15, color="green400"),
+                                ft.Text(value="Device ID", size=20, color="white",), 
+                            ],
+                        ),
+                    ),
                 ),
                 
                 ft.Container(
-                    ft.Switch(label=" ON/OFF", value=False, label_position=ft.LabelPosition.LEFT)
-                )
+                    ft.Switch(
+                        label=" ON/OFF", 
+                    value=False, 
+                    label_position=ft.LabelPosition.LEFT, 
+                    active_track_color="green",
+                    
+                    )
+
+                ),
+                
                 
             ]
         )
@@ -50,43 +62,37 @@ def main(page: ft.Page):
 
 
     second_row = ft.Container(
-        content=ft.Column(
-            [
-                ft.Row(
-                    alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
-                    controls=[
-                        create_color_units(ft.colors.RED, [255,0,0]),
-                        create_color_units(ft.colors.GREEN, [0,255,0]),
-                        create_color_units(ft.colors.BLUE, [0,0,255]),
-                        create_color_units(ft.colors.BLACK, [0,0,0]),
-                        
+        margin=ft.margin.all(15),
+        content=ft.GridView(
+                runs_count=3,
+                max_extent=width/4,
+                padding=ft.padding.only(left=40, right=40),
+                controls=[
+                    create_color_units("red",[255,255,255]),
+                    create_color_units("green",[255,255,255]),
+                    create_color_units("blue",[255,255,255]),
+                    create_color_units("orange",[255,255,255]),
+                    create_color_units("teal",[255,255,255]),
+                    create_color_units("cyan",[255,255,255]),
+                    create_color_units("white",[255,255,255]),
+                    create_color_units("yellow",[255,255,255]),
+                    create_color_units("pink",[255,255,255]),
                     ]
-                ),
+            )
 
-                ft.Row(
-                    alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
-                    controls=[
-                        create_color_units(ft.colors.RED, [255,0,0]),
-                        create_color_units(ft.colors.GREEN, [0,255,0]),
-                        create_color_units(ft.colors.BLUE, [0,0,255]),
-                        create_color_units(ft.colors.BLACK, [0,0,0]),
-
-                    ]
-                ),
-
-            ]
         )
 
-    )
 
     third_row = ft.Container(
+        margin=ft.margin.all(15),
+        padding=ft.padding.only(left=40, right=40),
         content= ft.Row
         (
             alignment=ft.MainAxisAlignment.CENTER,
             controls=[
                ft.ElevatedButton(
                 expand = True,
-                height = width/6,
+                height = width/5,
                 style=ft.ButtonStyle(
                     bgcolor = ft.colors.WHITE,
                     elevation = 0,
@@ -103,19 +109,25 @@ def main(page: ft.Page):
         pass
 
     fourth_row = ft.Container(
+        margin=ft.margin.all(15),
+        padding=ft.padding.only(left=40, right=40),
         content=ft.Column(
+            
             controls=[
                 ft.Row(
                     alignment=ft.MainAxisAlignment.CENTER,
+                    
                     controls=[
                         ft_color_picker.HueSlider(on_change_hue=change_hue),
-                    ]
+                    ],
+                    
                 ),
+
                 ft.Row(
                     alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
                     controls=[
-                        ft.Icon(name=ft.icons.BRIGHTNESS_LOW, color=ft.colors.WHITE),
-                        ft.Slider(expand=True),
+                        ft.Icon(name=ft.icons.BRIGHTNESS_LOW, color=ft.colors.WHITE70),
+                        ft.Slider(expand=True, active_color=ft.colors.WHITE),
                         ft.Icon(name=ft.icons.BRIGHTNESS_HIGH, color=ft.colors.WHITE),
                     ]
                 ),
@@ -123,7 +135,7 @@ def main(page: ft.Page):
                     alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
                     controls=[
                         ft.Icon(name=ft.icons.FAST_FORWARD_OUTLINED, color=ft.colors.WHITE),
-                        ft.Slider(expand=True),
+                        ft.Slider(expand=True, active_color=ft.colors.WHITE),
                         ft.Icon(name=ft.icons.FAST_FORWARD, color=ft.colors.WHITE),
                     ]
                 ),
@@ -132,6 +144,7 @@ def main(page: ft.Page):
     )
 
     last_row = ft.Container(
+        margin=ft.margin.only(top=15, left=15,right=15,bottom=5),
         content=ft.Row(
             controls=[
                 ft.Dropdown(
@@ -163,7 +176,7 @@ def main(page: ft.Page):
     main_container = ft.Container(
         width = width,
         height = height,
-        bgcolor = ft.colors.GREY_800,
+        #bgcolor = ft.colors.GREY_800,
         #border_radius = ft.border_radius.only(top_left=30, top_right=30, bottom_left=0, bottom_right=0),
         margin=ft.margin.symmetric(vertical=10),
         padding = ft.padding.all(10),
