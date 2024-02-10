9bc9979889dbf3008c1807b6984e5e77005f644f
Kleiton Andrade <Kleiton1002andrade@gmail.com>
2024-02-10 18:42:53 -0100

    Scanning Devices Done

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import flet as ft
 import flet_contrib.color_picker as ft_color_picker
+import asyncio
+from bleak import BleakScanner
 
 
 
@@ -22,19 +24,42 @@ def main(page: ft.Page):
     
     def close_dlg(e):
 
-        device_id_text.current.value = e.control.data
+        device_id_text.current.value = e.control.data[0]
         dlg_modal.open = False
         page.update()
 
-    device1 = {
-        "ID":"test_id1",
-    }
+    def open_dlg_modal(e):
 
-    device2 = {
-        "ID":"test_id2",
-    }
+            dlg_column.current.controls.clear()
 
-    devices = [device1, device2,]
+            dlg_column.current.controls.append(ft.Row([ft.ProgressRing(width=16, height=16, stroke_width = 2), ft.Text("Scanning Devices")]),)
+            page.dialog = dlg_modal
+            dlg_modal.open = True
+            page.update()
+
+            devices = asyncio.run(main())
+
+            if devices != []:
+
+                dlg_column.current.controls.clear()
+
+                for device in devices:
+                    dlg_column.current.controls.append(
+                        ft.TextButton(f"{device['ID']}", on_click=close_dlg, data=[f"{device['ID']}",f"{device['address']}"])
+                    )
+                
+                dlg_column.current.height = 35*len(devices) + 5
+
+                page.dialog = dlg_modal
+                dlg_modal.open = True
+                page.update()
+            
+            else:
+                dlg_column.current.controls.clear()
+                dlg_column.current.controls.append(ft.Text("NO DEVICES"))
+                page.dialog = dlg_modal
+                dlg_modal.open = True
+                page.update()
 
     dlg_modal = ft.AlertDialog(
         modal=True,
@@ -46,29 +71,30 @@ def main(page: ft.Page):
             ]
         ),
         actions=[
-            ft.TextButton("Cancel", on_click=close_dlg, data="Device ID"),
+            ft.TextButton("Cancel", on_click=close_dlg, data=[f"Connect Device",f""]),
+            ft.TextButton("Re-Scan", on_click=open_dlg_modal,),
         ],
         actions_alignment=ft.MainAxisAlignment.END,
-        on_dismiss=lambda e: print("Modal dialog dismissed!"),
+        #on_dismiss=lambda e: print("Modal dialog dismissed!"),
     )
 
-    def open_dlg_modal(e):
+    async def main():
 
-        dlg_column.current.controls.clear()
+        device_list = []
 
-        for device in devices:
-            dlg_column.current.controls.append(
-                ft.TextButton(f"{device['ID']}", on_click=close_dlg, data=f"{device['ID']}")
-            )
-        
-        dlg_column.current.height = 35*len(devices) + 5
-        
-        dlg_column.current.width = 2*len(devices)
+        devices = await BleakScanner.discover()
+        if devices != None:
+            for d in devices:
+            #print(d.address)
 
-        page.dialog = dlg_modal
-        dlg_modal.open = True
-        page.update()
+                device_item = {
+                    "ID":f"{d.name}",
+                    "address":f"{d.address}"
+                }
 
+            device_list.append(device_item)
+    
+        return device_list
 
     first_row = ft.Container(
         margin=ft.margin.only(top=5, left=15,right=15,bottom=15),
@@ -79,8 +105,8 @@ def main(page: ft.Page):
                     content=ft.Container(
                         content=ft.Row(
                             [
-                                ft.Icon(name="circle",size=15, color="green400"),
-                                ft.Text(ref= device_id_text, value="Device ID", size=20, color="white",), 
+                                ft.Icon(name="circle",size=15, color="red400"),
+                                ft.Text(ref= device_id_text, value="Connect Device", size=20, color="white",), 
                             ],
                         ),
                     ),
